ea678f9f8ded73fe4cbd2bf74c99f472eea478d2
Rustam Urinboyev <rustamurinboyev@MacBook-Air.local>
2026-06-05 04:07:12 +0500

    chore: use postgresql prisma client in production

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
 /**
  * Объявление глобальной переменной для предотвращения создания множества экземпляров PrismaClient
@@ -8,7 +9,12 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-// В продакшене (Vercel) используем стандартный PrismaClient для PostgreSQL
-export const prisma = global.prisma || new PrismaClient();
+export const prisma =
+  global.prisma ||
+  (process.env.NODE_ENV === "production"
+    ? new PrismaClient()
+    : new PrismaClient({
+        adapter: new PrismaBetterSqlite3({ url: process.env.DATABASE_URL || "file:./dev.db" }),
+      }));
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
